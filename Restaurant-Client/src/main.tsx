fedf0418bd2d9f71fccc43b28c1c1a35d31c34f0
Spas <Vutovspas@gmail.com>
2017-04-17 18:16:34 +0300

    Added sample react client

---
+++ Restaurant-Client/src/main.tsx
@@ -0,0 +1,22 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import {Router, Route, HashRouter} from 'react-router-dom';
+import {createHashHistory} from 'history';
+import {PageA} from './components/pageA';
+import {PageB} from './components/pageB';
+
+const history = createHashHistory();
+
+ReactDOM.render(
+  <HashRouter>
+    <Router history={history} >    
+        <div>    
+          <Route exact={true} path="/" component={PageA}/>
+          <Route path="/pageB" component={PageB}/>
+        </div>
+    </Router>    
+  </HashRouter>
+
+,
+  document.getElementById('root')
+);
